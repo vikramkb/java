5eb0d17c79bf4dc4720f2fb491f3bf302ab0335d
vikram K <vikramkb@thoughtworks.com>
2017-02-12 18:36:01 +0530

    add atomic variables example

---
+++ Threads/src/atomic/AtomicTypes.java
@@ -0,0 +1,82 @@
+package atomic;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+class Count {
+    private AtomicInteger intCount = new AtomicInteger();
+    private AtomicBoolean boolCount = new AtomicBoolean();
+    private AtomicLong longCount = new AtomicLong();
+
+    Count() {
+        intCount.set(100);
+        boolCount.set(true);
+        longCount.set(200);
+    }
+    public void increment() {
+        intCount.getAndIncrement();
+        boolCount.getAndSet(true);
+        longCount.getAndIncrement();
+    }
+
+    public void decrement() {
+        intCount.getAndDecrement();
+        boolCount.getAndSet(false);
+        longCount.getAndDecrement();
+    }
+
+    @Override
+    public String toString() {
+        return "Atomic int count = " + intCount.toString() + "\n" +
+        "Atomic bool status = " + boolCount.toString() + "\n" +
+        "Atomic long count = " + longCount.toString() + "\n";
+    }
+}
+
+class CoinCount implements Callable {
+    public static Count count = new Count();
+    private boolean isIncrement;
+
+    public CoinCount(boolean isIncrement) {
+        this.isIncrement = isIncrement;
+    }
+
+    @Override
+    public Integer call() {
+        if(isIncrement){
+            count.increment();
+        }else {
+            count.decrement();
+        }
+        return 0;
+    }
+}
+
+public class AtomicTypes {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Initial values before thread operations");
+        System.out.println(CoinCount.count);
+        System.out.println("Should be same as initial values after same number of increments and decrements");
+
+        List<Callable<Integer>> threads = new LinkedList<>();
+        for(int i=0; i < 2; i++)
+            threads.add(new CoinCount((i%2)==0));
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        threadPool.invokeAll(threads);
+
+        Thread.sleep(1000);
+        System.out.println(CoinCount.count);
+        Thread.sleep(1000);
+        System.out.println(CoinCount.count);
+        Thread.sleep(1000);
+        System.out.println(CoinCount.count);
+        threadPool.shutdown();
+    }
+}
---
+++ Threads/src/atomic/WithOutAtomicCount.java
@@ -0,0 +1,81 @@
+package atomic;
+
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Count1 {
+    private int intCount = 0;
+    private boolean boolCount = false;
+    private long longCount = 0;
+
+    Count1() {
+        intCount = 100;
+        boolCount = true;
+        longCount = 200;
+    }
+
+
+    public void increment() {
+        intCount++;
+        boolCount = true;
+        longCount++;
+    }
+
+    public void decrement() {
+        intCount--;
+        boolCount = false;
+        longCount--;
+    }
+
+    @Override
+    public String toString() {
+        return "Non atomic int count = " + intCount + "\n" +
+                "Non atomic bool status = " + boolCount + "\n" +
+                "Non atomic long count = " + longCount + "\n";
+    }
+}
+
+class CoinCount1 implements Callable {
+    public static Count1 count = new Count1();
+    private boolean isIncrement;
+
+    public CoinCount1(boolean isIncrement) {
+        this.isIncrement = isIncrement;
+    }
+
+    @Override
+    public Integer call() {
+        if(isIncrement){
+            count.increment();
+        }else {
+            count.decrement();
+        }
+        return 0;
+    }
+}
+
+public class WithOutAtomicCount {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Initial values before thread operations");
+        System.out.println(CoinCount1.count);
+        System.out.println("Should be same as initial values after same number of increments and decrements");
+        System.out.println("Run multiple types. If you are lucky sometimes you may get expected values.");
+
+        List<Callable<Integer>> threads = new LinkedList<>();
+        for(int i=0; i < 10000; i++)
+            threads.add(new CoinCount1(i%2==0));
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        threadPool.invokeAll(threads);
+
+        Thread.sleep(1000);
+        System.out.println(CoinCount1.count);
+        Thread.sleep(1000);
+        System.out.println(CoinCount1.count);
+        Thread.sleep(1000);
+        System.out.println(CoinCount1.count);
+        threadPool.shutdown();
+    }
+}
